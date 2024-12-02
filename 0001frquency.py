ba873536c184cfcb886f808ec8d8b5af492ab3e0
Asheen26 <149300176+Asheen26@users.noreply.github.com>
2024-12-02 11:47:41 +0530

    Add files via upload

---
+++ 0001frquency.py
@@ -0,0 +1,17 @@
+
+
+string=input("enter a string: ")
+
+frequency={}
+
+for char in string:
+    if char in frequency:
+        frequency[char]+=1
+
+    else:
+        frequency[char]=1    
+ 
+
+  
+
+print(frequency) 
